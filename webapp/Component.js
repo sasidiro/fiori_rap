bd6581859a99ac36fa1c8bd5efe5d564a5443ae9
Savvas Sidiropoulos <sasidiro@yahoo.gr>
2024-01-20 15:43:00 +0000

    test

---
+++ webapp/Component.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("project1.Component", {
+            metadata: {
+                manifest: "json"
+                
+            }
+        });
+    }
+);
